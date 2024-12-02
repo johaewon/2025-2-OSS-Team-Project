3223d7d301770b60ccb4f910a2db10831570aee5
조하영 <johy3943@naver.com>
2024-12-02 12:05:00 +0900

    Add files via upload

---
+++ preprocessing_utils.py
@@ -0,0 +1,22 @@
+import pygame
+from PIL import Image
+from torchvision import transforms
+
+# 음성 파일 재생 함수 (pygame 사용)
+def play_audio(audio_path):
+    pygame.mixer.init()
+    pygame.mixer.music.load(audio_path)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        continue
+    pygame.mixer.quit()
+
+# 이미지 전처리 함수
+def transform_image(image_path):
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    image = Image.open(image_path).convert('RGB')
+    return transform(image).unsqueeze(0)
